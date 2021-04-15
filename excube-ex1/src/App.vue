ea45bef934b449f4715b73b9d698afe3c73a3bcd
Ugo de Féligonde <ugo.defeli@gmail.com>
2021-04-15 12:08:54 +0200

    adding instructions

--- excube-ex1/src/App.vue
+++ excube-ex1/src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img width="40%" src="./assets/logo_augmentedciso.png" />
+    <p>
+      Votre objectif: développer un composant permettant de représenter une
+      matrice comme celle ci-dessous.
+    </p>
+    <img width="90%" src="./assets/gitlab.png" />
+
+    <hr />
+    <hr />
+
+    <p>
+      Cette image a été généré à partir des informations contenus dans le
+      fichier data.json (répertoire assets). Pour chaque jour, le nombre de
+      contributions est indiqué en entier. Les dates sans contribution ne sont
+      pas inclues dans le fichier.
+    </p>
+    <h5>Ta mission</h5>
+    <ul>
+      <li>
+        Développer un composant uniquement en VueJS (pas de d3 ou autre)
+        permettant d'afficher une vue web similaire à l'image
+      </li>
+      <li>Tu peux bien entendu développer plusieurs composants si besoin</li>
+      <li>
+        Au passage de la souris sur une case, on doit voir le nombre exacte de
+        contributions
+      </li>
+      <li>
+        L'échelle (en bas à gauche) doit être visible, elle est statique et
+        voici les infos à utiliser
+        <ul>
+          <li>#ededed: 0 contributions</li>
+          <li>#acd6f2: 1-9 contributions</li>
+          <li>#7fa8c9: 10-19 contributions</li>
+          <li>#527ba0: 20-29 contributions</li>
+          <li>#264e77: 30+ contributions</li>
+        </ul>
+      </li>
+      <li>
+        Il est possible (mais pas indispensable) d'utiliser la library moment
+        pour gérer les dates.
+      </li>
+    </ul>
+    <p>Bon courage :)</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#app p,
+#app ul {
+  text-align: left;
 }
 </style>
